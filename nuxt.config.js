92eda5944ed19077a0fc877e4ab84a05c001217e
Clint.Network <me@clint.network>
2021-05-27 19:57:02 +0300

    Create MVP

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,57 @@
 export default {
-  target: 'static',
+	target: 'static',
 
 	generate: {
 		dir: 'public'
 	},
-  
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'hypespeed.co',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: 'HypeSpeed.co | Speed Test Powered by HypeProxy.io',
+		htmlAttrs: {
+			lang: 'en'
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/svg', href: '/favicon.svg' },
+			{ rel: 'stylesheet', type: 'text/css', href: 'https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' }
+		]
+	},
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [
+		"~/assets/app.scss"
+	],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	env: {
+		baseUrl: process.env.BASE_URL || 'https://localhost:7001/v2/',
+	},
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+	axios: {
+		baseURL: process.env.BASE_URL || 'https://localhost:7001/v2/'
+	},
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [
+	],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
+
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+	],
+
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		'@nuxtjs/axios'
+	],
+
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {
+	}
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,72 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        hypespeed.co
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="text-center">
+<!--	  <img :src="require('~/assets/hype-min.png')" class="mb-4" alt="hypeproxy.io logo" />-->
+	  <h1 v-if="status === 100" class="status fw-bold">
+		  {{ getSpeed() }} MB/s
+	  </h1>
+	  <h1 v-else class="status fw-bold">
+		  {{ status }}%
+	  </h1>
+	  <p v-if="timespan === 0" class="lead text-muted">Downloading 100 megabytes of random data.</p>
+	  <p v-else class="lead text-muted">
+		  Successfully completed speedtest, 100Mb downloaded in {{ test() }}.
+	  </p>
+<!--	  <h1 class="display-1 fw-bold">HypeSpeed.co</h1>-->
+<!--	  <p class="h3 fw-light mb-4">A fast.com alternative powered by HypeProxy.io</p>-->
+<!--	  <div class="mb-5">-->
+<!--		  <a class="btn btn-dark btn-lg" href="https://medium.com/@clintnetwork/lucid-an-http-key-value-store-c0e734586e26">-->
+<!--			  <i class="bx bxs-hot me-1"></i> Visit HypeProxy.io-->
+<!--		  </a>-->
+<!--		  <a class="btn btn-outline-dark btn-lg" href="https://docs.lucid-kv.store/">-->
+<!--			  <i class="fas fa-book"></i>&nbsp; Official Documentation-->
+<!--		  </a>-->
+<!--	  </div>-->
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+	data() {
+		return {
+			status: 0,
+			timespan: 0
+		}
+	},
+	async mounted() {
+		let startDate = performance.now();
+		await this.$axios.get('Insight/SpeedTest/100mb', {
+			onDownloadProgress: progressEvent => {
+				const total = parseFloat(progressEvent.currentTarget.getResponseHeader('Content-Length'))
+				const current = progressEvent.currentTarget.response.length
+				this.status = Math.floor(current / total * 100)
+			}
+		}).then(res => {
+			this.status = 100;
+			let endDate = performance.now()
+			this.timespan = endDate - startDate;
+			return res.data;
+		})
+	},
+	methods: {
+		test: function () {
+			const humanizeDuration = require("humanize-duration");
+			return humanizeDuration(this.timespan, { round: true });
+		},
+		getSpeed: function () {
+			return (100 / this.timespan * 1000).toFixed(0);
+		}
+	}
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+img
+{
+	width: 177px;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.status
+{
+	font-size: 10em;
 }
 </style>
